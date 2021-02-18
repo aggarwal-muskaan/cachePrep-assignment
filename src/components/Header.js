da0d4b086d3eebf26b314775f3c77475fe7d4d9d
Muskaan Aggarwal <muskaan19010@gmail.com>
2021-02-18 19:45:03 +0530

    style: add badge to cart icon

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useContext } from "react";
+import { cartContext } from "../contexts/cart.context";
 import { Link } from "react-router-dom";
 
 import "../styles/HeaderStyle.css";
@@ -7,12 +8,16 @@ import { faCartPlus } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 export default function Header() {
+  const currentProducts = useContext(cartContext);
+  const badgeCount = currentProducts.length;
   return (
     <div className="Header">
       <h1>Header</h1>
+
+      {/* fix shifting of badgeCount on drawer open */}
       <div className="Header-cart-icon">
-        <Link to="/checkout">
-          <FontAwesomeIcon icon={faCartPlus} size="2x" />
+        <Link to="/checkout" data-count={badgeCount}>
+          <FontAwesomeIcon icon={faCartPlus} size="2x" className="badge" />
         </Link>
       </div>
     </div>

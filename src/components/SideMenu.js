048bc825afae7f2faef75c44c1959c433bd4caaf
Muskaan Aggarwal <muskaan19010@gmail.com>
2021-02-18 12:12:55 +0530

    add button to side menu

--- src/components/SideMenu.js
+++ src/components/SideMenu.js
@@ -5,22 +5,25 @@ import "../styles/DrawerStyle.css";
 
 export default function SideMenu() {
   return (
-    <div className="Side-menu">
-      <h3 className="container-heading">Subscription Plans</h3>
+    <>
+      <div className="Side-menu">
+        <h3 className="container-heading">Subscription Plans</h3>
 
-      {plans.map((p) => (
-        <div className="plan-card" key={p.time}>
-          <h2 className="plan-card-duration">{p.time}</h2>
-          {p.recommended && (
-            <small className="default-plan-text">(Recommended)</small>
-          )}
-          <h3 className="plan-card-cost">&#8377;{p.actualCost}</h3>
-          <small className="plan-card-strike">&#8377;{p.cost}</small>
-          <div className="plan-add-button">
-            <button className="card-button">CHOOSE</button>
+        {plans.map((p) => (
+          <div className="plan-card" key={p.time}>
+            <h2 className="plan-card-duration">{p.time}</h2>
+            {p.recommended && (
+              <small className="default-plan-text">(Recommended)</small>
+            )}
+            <h3 className="plan-card-cost">&#8377;{p.actualCost}</h3>
+            <small className="plan-card-strike">&#8377;{p.cost}</small>
+            <div className="plan-add-button">
+              <button className="card-button">CHOOSE</button>
+            </div>
           </div>
-        </div>
-      ))}
-    </div>
+        ))}
+      </div>
+      <button className="view-cart-button">View Cart</button>
+    </>
   );
 }

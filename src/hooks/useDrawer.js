81b2365fae65b5907b6378e44a89019931e61b19
Muskaan Aggarwal <muskaan19010@gmail.com>
2021-02-18 16:01:18 +0530

    add default value

--- src/hooks/useDrawer.js
+++ src/hooks/useDrawer.js
@@ -2,7 +2,7 @@ import { useState } from "react";
 
 export default function useDrawer(init) {
   const [value, setValue] = useState({ open: init, courseId: null });
-  const showDrawer = (val, id) => {
+  const showDrawer = (val, id = null) => {
     setValue({ open: val, courseId: id });
   };
 

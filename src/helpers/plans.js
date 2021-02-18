81defd834d1f8ee16ac1f8625cd6516cd5d9294e
Muskaan Aggarwal <muskaan19010@gmail.com>
2021-02-18 17:29:17 +0530

    add id attribute to objects

--- src/helpers/plans.js
+++ src/helpers/plans.js
@@ -3,18 +3,21 @@
 // subscription plan
 export default [
   {
+    id: "m1",
     time: "1 Month",
     actualCost: "4,999",
     cost: "6,999",
     recommended: false,
   },
   {
+    id: "m3",
     time: "3 Months",
     actualCost: "11,999",
     cost: "19,999",
     recommended: true,
   },
   {
+    id: "m6",
     time: "6 Months",
     actualCost: "19,999",
     cost: "40,999",

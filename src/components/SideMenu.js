bb96c4889f9fdc34d51580d1302dce8a8d7e0de4
Muskaan Aggarwal <muskaan19010@gmail.com>
2021-02-18 17:37:54 +0530

    feat: add logic to CHOOSE button

--- src/components/SideMenu.js
+++ src/components/SideMenu.js
@@ -1,16 +1,19 @@
-import React from "react";
+import React, { useContext } from "react";
+import { dispatchContext } from "../contexts/cart.context";
 import plans from "../helpers/plans";
-
 import "../styles/DrawerStyle.css";
 
-export default function SideMenu() {
+export default function SideMenu({ courseId }) {
+  // provide method for editing subscription plans
+  const dispatch = useContext(dispatchContext);
+
   return (
     <>
       <div className="Side-menu">
         <h3 className="container-heading">Subscription Plans</h3>
 
         {plans.map((p) => (
-          <div className="plan-card" key={p.time}>
+          <div className="plan-card" key={p.id}>
             <h2 className="plan-card-duration">{p.time}</h2>
             {p.recommended && (
               <small className="default-plan-text">(Recommended)</small>
@@ -18,7 +21,14 @@ export default function SideMenu() {
             <h3 className="plan-card-cost">&#8377;{p.actualCost}</h3>
             <small className="plan-card-strike">&#8377;{p.cost}</small>
             <div className="plan-add-button">
-              <button className="card-button">CHOOSE</button>
+              <button
+                onClick={() =>
+                  dispatch({ type: "editPlan", id: courseId, plan: p.id })
+                }
+                className="card-button"
+              >
+                CHOOSE
+              </button>
             </div>
           </div>
         ))}

8a8580f9b85f4a63acb7380721708f2dc6f7e8f2
Muskaan Aggarwal <muskaan19010@gmail.com>
2021-02-18 12:02:12 +0530

    edit: add heading to side menu

--- src/components/SideMenu.js
+++ src/components/SideMenu.js
@@ -6,8 +6,10 @@ import "../styles/DrawerStyle.css";
 export default function SideMenu() {
   return (
     <div className="Side-menu">
+      <h3 className="container-heading">Subscription Plans</h3>
+
       {plans.map((p) => (
-        <div className="plan-card">
+        <div className="plan-card" key={p.time}>
           <h2 className="plan-card-duration">{p.time}</h2>
           {p.recommended && (
             <small className="default-plan-text">(Recommended)</small>
@@ -15,7 +17,7 @@ export default function SideMenu() {
           <h3 className="plan-card-cost">&#8377;{p.actualCost}</h3>
           <small className="plan-card-strike">&#8377;{p.cost}</small>
           <div className="plan-add-button">
-            <button className="card-button">ADD</button>
+            <button className="card-button">CHOOSE</button>
           </div>
         </div>
       ))}
